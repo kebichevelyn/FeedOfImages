441711e96b8e9bd75e0834964837a1bd2783136f
evelina kebich <KebichEvelina@yandex.by>
2025-08-10 10:27:56 +0300

    Initial Commit

---
+++ FeedOfImages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FeedOfImages
+//
+//  Created by Evelina Kebich on 10.08.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
