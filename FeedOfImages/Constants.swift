b78d041336dbee04fb0eb5bd588f8d556bbae9dd
evelina kebich <KebichEvelina@yandex.by>
2025-09-05 13:20:32 +0300

    добавлены константы

---
+++ FeedOfImages/Constants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Constants {
+    static let accessKey = "eVDMtRMJDuqPF1swn9g41VAyiEInUFGeBncwECEOm0E"
+    static let secretKey = "ci_UDDyeXPnv36BnsiZvU0L_JM2nwyK8xs-d5ZSWeh4"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
