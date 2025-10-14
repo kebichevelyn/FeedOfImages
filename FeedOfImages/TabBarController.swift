c4ba3ebbf6b116882504f73351cd99675b7b8b47
evelina kebich <KebichEvelina@yandex.by>
2025-10-14 22:26:15 +0300

    Финальная версия awakeFromNib

--- FeedOfImages/TabBarController.swift
+++ FeedOfImages/TabBarController.swift
@@ -1,8 +1,17 @@
-//
-//  TabBarController.swift
-//  FeedOfImages
-//
-//  Created by Evelina Kebich on 14.10.25.
-//
+import UIKit
 
-import Foundation
+final class TabBarController: UITabBarController {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        let imagesListViewController = storyboard.instantiateViewController(withIdentifier: "ImagesListViewController")
+        let profileViewController = ProfileViewController()
+        profileViewController.tabBarItem = UITabBarItem(
+            title: "",
+            image: UIImage(named: "tab_profile_active"),
+            selectedImage: nil
+        )
+        self.viewControllers = [imagesListViewController, profileViewController]
+    }
+}
+
