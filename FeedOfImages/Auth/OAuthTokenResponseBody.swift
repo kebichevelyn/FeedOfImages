7ad2eb0d78fb46937e0d6d8b6bce127a5f8803e6
evelina kebich <KebichEvelina@yandex.by>
2025-10-15 20:42:18 +0300

    вторая попытка ревью

--- FeedOfImages/Auth/OAuthTokenResponseBody.swift
+++ FeedOfImages/Auth/OAuthTokenResponseBody.swift
@@ -8,23 +8,9 @@ struct OAuthTokenResponseBody: Decodable {
     let scope: String
     let createdAt: Int
     
-   // private enum CodingKeys: String, CodingKey {
-       // case accessToken = "access_token"
-        //case tokenType = "token_type"
-        //case scope
-        //case createdAt = "created_at"
-    //}
     
     private enum ParseError: Error {
         case createdAtFailure
     }
-    
-   // init(from decoder: Decoder) throws {
-        //let container = try decoder.container(keyedBy: CodingKeys.self)
-        //accessToken = try container.decode(String.self, forKey: .accessToken)
-        //tokenType = try container.decode(String.self, forKey: .tokenType)
-        //scope = try container.decode(String.self, forKey: .scope)
-        //createdAt = try container.decode(Date.self, forKey: .createdAt)
-    //}
 }
 
