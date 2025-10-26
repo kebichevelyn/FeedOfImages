5f6df6667eb3931c5c46e2610b533afc95164da3
evelina kebich <KebichEvelina@yandex.by>
2025-10-26 13:41:36 +0300

    сохранение изменений 11 спринта

---
+++ FeedOfImages/ImagesList/ImagesListService.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+final class ImagesListService {
+    private(set) var photos: [Photo] = []
+    
+    private var lastLoadedPage: Int?
+    
+    // ...
+    
+    func fetchPhotosNextPage() {
+        // ...
+    }
+}
+
+struct Photo {
+    let id: String
+    let size: CGSize
+    let createdAt: Date?
+    let welcomeDescription: String?
+    let thumbImageURL: String
+    let largeImageURL: String
+    let isLiked: Bool
+} 
+
