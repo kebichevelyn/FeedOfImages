bada93405ba1ab0797987f6817953df766a8eca8
evelina kebich <KebichEvelina@yandex.by>
2025-11-10 17:13:13 +0300

    func tableView

--- FeedOfImages/ImagesList/ImagesListCell.swift
+++ FeedOfImages/ImagesList/ImagesListCell.swift
@@ -6,11 +6,6 @@ final class ImagesListCell: UITableViewCell {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var likeButton: UIButton!
-    
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        cellImage.kf.cancelDownloadTask()
-    }
 }
 
    
--- FeedOfImages/ViewControllers/ImagesListViewController.swift
+++ FeedOfImages/ViewControllers/ImagesListViewController.swift
@@ -1,117 +1,5 @@
-//import UIKit
-//import WebKit
-//import Kingfisher
-//
-//final class ImagesListViewController: UIViewController {
-//    private let showSingleImageSegueIdentifier = "ShowSingleImage"
-//
-//    @IBOutlet private var tableView: UITableView!
-//    
-//    private var photos: [Photo] = []
-//    
-//    private lazy var dateFormatter: DateFormatter = {
-//        let formatter = DateFormatter()
-//        formatter.dateStyle = .long
-//        formatter.timeStyle = .none
-//        return formatter
-//    }()
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        tableView.rowHeight = 200
-//        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
-//
-//        loadNextPage()
-//    }
-//    
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        if segue.identifier == showSingleImageSegueIdentifier {
-//            guard
-//                let viewController = segue.destination as? SingleImageViewController,
-//                let indexPath = sender as? IndexPath
-//            else {
-//                assertionFailure("Invalid segue destination")
-//                return
-//            }
-////            viewController.image = image
-//        } else {
-//            super.prepare(for: segue, sender: sender)
-//        }
-//    }
-//}
-//
-//extension ImagesListViewController: UITableViewDataSource {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return photos.count
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
-//
-//        guard let imageListCell = cell as? ImagesListCell else {
-//            return UITableViewCell()
-//        }
-//
-//        configCell(for: imageListCell, with: indexPath)
-//
-//        return imageListCell
-//    }
-//}
-//
-//extension ImagesListViewController {
-//    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
-//        cell.cellImage.kf.setImage(with: photos[indexPath.row].fullImageURL)
-//        cell.dateLabel.text = dateFormatter.string(from: Date())
-//
-//        let isLiked = indexPath.row % 2 == 0
-//        let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
-//        cell.likeButton.setImage(likeImage, for: .normal)
-//    }
-//}
-//
-//extension ImagesListViewController: UITableViewDelegate {
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
-//    }
-//
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        let photo = photos[indexPath.row]
-//        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
-//        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
-//        let imageWidth = photo.size.width
-//        let scale = imageViewWidth / imageWidth
-//        let cellHeight = photo.size.height * scale + imageInsets.top + imageInsets.bottom
-//        return cellHeight
-//    }
-//    
-//    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-//        
-//        if indexPath.row + 1 == photos.count {
-//            print("loading next page")
-//            loadNextPage()
-//        }
-//    }
-//}
-//
-//
-//private extension ImagesListViewController {
-//    func loadNextPage() {
-//        ImagesListService.shared.fetchPhotosNextPage { [weak self] result in
-//            guard let self else { return }
-//            switch result {
-//            case let .success(newPhotos):
-//                photos += newPhotos
-//                tableView.reloadData()
-//            case .failure:
-//                return
-//            }
-//        }
-//    }
-//}
-
-
 import UIKit
+import WebKit
 import Kingfisher
 
 final class ImagesListViewController: UIViewController {
@@ -134,19 +22,7 @@ final class ImagesListViewController: UIViewController {
         tableView.rowHeight = 200
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
 
-        NotificationCenter.default.addObserver(
-            forName: ImagesListService.didChangeNotification,
-            object: nil,
-            queue: .main
-        ) { [weak self] _ in
-            self?.updateTableViewAnimated()
-        }
-        
         loadNextPage()
-    }
-    
-    deinit {
-        NotificationCenter.default.removeObserver(self)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -185,19 +61,11 @@ extension ImagesListViewController: UITableViewDataSource {
 
 extension ImagesListViewController {
     func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
-        let photo = photos[indexPath.row]
-        
-        cell.cellImage.kf.indicatorType = .activity
-        let placeholder = UIImage(named: "stub_image")
-        cell.cellImage.kf.setImage(with: photo.fullImageURL, placeholder: placeholder)
-        
-        if let createdAt = photo.createdAt {
-            cell.dateLabel.text = dateFormatter.string(from: createdAt)
-        } else {
-            cell.dateLabel.text = ""
-        }
-        
-        let likeImage = photo.isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
+        cell.cellImage.kf.setImage(with: photos[indexPath.row].fullImageURL)
+        cell.dateLabel.text = dateFormatter.string(from: Date())
+
+        let isLiked = indexPath.row % 2 == 0
+        let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
         cell.likeButton.setImage(likeImage, for: .normal)
     }
 }
@@ -218,37 +86,26 @@ extension ImagesListViewController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        if indexPath.row + 1 == photos.count {
-            print("loading next page")
-            loadNextPage()
-        }
+        
+        if indexPath.row + 1 == ImagesListService.shared.photos.count {
+                ImagesListService.shared.fetchPhotosNextPage { _ in }
+            }
     }
 }
+
 
 private extension ImagesListViewController {
     func loadNextPage() {
         ImagesListService.shared.fetchPhotosNextPage { [weak self] result in
-            guard let self = self else { return }
+            guard let self else { return }
             switch result {
-            case .success:
-                break
+            case let .success(newPhotos):
+                photos += newPhotos
+                tableView.reloadData()
             case .failure:
                 return
             }
         }
     }
-    
-    func updateTableViewAnimated() {
-        let oldCount = photos.count
-        let newCount = ImagesListService.shared.getPhotos().count
-        
-        photos = ImagesListService.shared.getPhotos()
-        
-        if oldCount != newCount {
-            tableView.performBatchUpdates {
-                let indexPaths = (oldCount..<newCount).map { IndexPath(row: $0, section: 0) }
-                tableView.insertRows(at: indexPaths, with: .automatic)
-            } completion: { _ in }
-        }
-    }
 }
+
