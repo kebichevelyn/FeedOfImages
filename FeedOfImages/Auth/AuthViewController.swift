56961628ba8086b3abead2e28dc463f98ffbd59d
evelina kebich <KebichEvelina@yandex.by>
2025-11-21 12:35:52 +0300

    хелперы

--- FeedOfImages/Auth/AuthViewController.swift
+++ FeedOfImages/Auth/AuthViewController.swift
@@ -20,21 +20,39 @@ final class AuthViewController: UIViewController {
         configureBackButton()
     }
     
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == showWebViewSegueIdentifier {
+//            
+//            guard !isAuthenticating else {
+//                return
+//            }
+//            
+//            guard
+//                let webViewViewController = segue.destination as? WebViewViewController
+//            else {
+//                assertionFailure("Failed to prepare for \(showWebViewSegueIdentifier)")
+//                return
+//            }
+//            //webViewViewController.delegate = self
+//            let webViewPresenter = WebViewPresenter()
+//            webViewViewController.presenter = webViewPresenter
+//            webViewPresenter.view = webViewViewController
+//            webViewViewController.delegate = self
+//        } else {
+//            super.prepare(for: segue, sender: sender)
+//        }
+//    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == showWebViewSegueIdentifier {
-            
-            guard !isAuthenticating else {
-                return
-            }
-            
             guard
                 let webViewViewController = segue.destination as? WebViewViewController
             else {
                 assertionFailure("Failed to prepare for \(showWebViewSegueIdentifier)")
                 return
             }
-            //webViewViewController.delegate = self
-            let webViewPresenter = WebViewPresenter()
+            let authHelper = AuthHelper()
+            let webViewPresenter = WebViewPresenter(authHelper: authHelper)
             webViewViewController.presenter = webViewPresenter
             webViewPresenter.view = webViewViewController
             webViewViewController.delegate = self
---
+++ FeedOfImages/AuthConfiguration.swift
@@ -0,0 +1,53 @@
+//import Foundation
+//
+//enum Constants {
+//    static let accessKey = "eVDMtRMJDuqPF1swn9g41VAyiEInUFGeBncwECEOm0E"
+//    static let secretKey = "ci_UDDyeXPnv36BnsiZvU0L_JM2nwyK8xs-d5ZSWeh4"
+//    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+//    static let accessScope = "public+read_user+write_likes"
+//    static var defaultBaseURLGet: URL {
+//        guard let url = URL(string: "https://api.unsplash.com") else {
+//            preconditionFailure("Invalid URL")
+//        }
+//        return url
+//    }
+//}
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "your_Access_Key"
+    static let secretKey = "your_Secret_Key"
+    static let redirectURI = "your_Redirect_URI"
+    static let accessScope = "public+read_user+write_likes"
+
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+}
+
+struct AuthConfiguration {
+    let accessKey: String
+    let secretKey: String
+    let redirectURI: String
+    let accessScope: String
+    let defaultBaseURL: URL
+    let authURLString: String
+
+    init(accessKey: String, secretKey: String, redirectURI: String, accessScope: String, authURLString: String, defaultBaseURL: URL) {
+        self.accessKey = accessKey
+        self.secretKey = secretKey
+        self.redirectURI = redirectURI
+        self.accessScope = accessScope
+        self.defaultBaseURL = defaultBaseURL
+        self.authURLString = authURLString
+    }
+
+    static var standard: AuthConfiguration {
+        return AuthConfiguration(accessKey: Constants.accessKey,
+                                 secretKey: Constants.secretKey,
+                                 redirectURI: Constants.redirectURI,
+                                 accessScope: Constants.accessScope,
+                                 authURLString: Constants.unsplashAuthorizeURLString,
+                                 defaultBaseURL: Constants.defaultBaseURL)
+    }
+}
