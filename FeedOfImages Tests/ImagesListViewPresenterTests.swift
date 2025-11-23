91694f52782859bf0470d68929baf0533eb1ddd8
evelina kebich <KebichEvelina@yandex.by>
2025-11-23 15:58:31 +0300

    тесты для imagesLists

---
+++ FeedOfImages Tests/ImagesListViewPresenterTests.swift
@@ -0,0 +1,115 @@
+import Foundation
+import XCTest
+@testable import FeedOfImages
+
+final class ImagesListViewPresenterTests: XCTestCase {
+    
+    func testViewDidLoadCallsSetupMethods() {
+        // given
+        let presenter = ImagesListViewPresenter()
+        let view = ImagesListViewControllerSpy()
+        presenter.view = view
+        
+        // when
+        presenter.viewDidLoad()
+        
+        // then - проверяем что не падает
+        XCTAssertTrue(true)
+    }
+    
+    func testFetchPhotosNextPage() {
+        // given
+        let presenter = ImagesListViewPresenter()
+        
+        // when
+        presenter.fetchPhotosNextPage()
+        
+        // then - проверяем что не падает
+        XCTAssertTrue(true)
+    }
+    
+    func testChangeLike() {
+        // given
+        let presenter = ImagesListViewPresenter()
+        
+        // when
+        presenter.changeLike(for: "test_photo_id", isLike: true)
+        
+        // then - проверяем что не падает
+        XCTAssertTrue(true)
+    }
+    
+    func testPhotoAtIndexPathWithNoPhotos() {
+        // given
+        let presenter = ImagesListViewPresenter()
+        let indexPath = IndexPath(row: 0, section: 0)
+        
+        // when
+        let photo = presenter.photo(at: indexPath)
+        
+        // then
+        XCTAssertNil(photo)
+    }
+    
+    func testPhotosCountInitiallyZero() {
+        // given
+        let presenter = ImagesListViewPresenter()
+        
+        // when
+        let count = presenter.photosCount
+        
+        // then
+        XCTAssertEqual(count, 0)
+    }
+    
+    func testDidSelectRowWithNoPhoto() {
+        // given
+        let presenter = ImagesListViewPresenter()
+        let view = ImagesListViewControllerSpy()
+        presenter.view = view
+        let indexPath = IndexPath(row: 0, section: 0)
+        
+        // when
+        presenter.didSelectRow(at: indexPath)
+        
+        // then - не должно падать даже если фото нет
+        XCTAssertFalse(view.showSingleImageViewControllerCalled)
+    }
+    
+    func testPresenterInitialization() {
+        // given
+        let presenter = ImagesListViewPresenter()
+        
+        // then
+        XCTAssertNotNil(presenter)
+    }
+    
+    func testViewConnection() {
+        // given
+        let presenter = ImagesListViewPresenter()
+        let view = ImagesListViewControllerSpy()
+        
+        // when
+        presenter.view = view
+        
+        // then
+        XCTAssertTrue(presenter.view === view)
+    }
+    
+    func testNotificationHandling() {
+        // given
+        let presenter = ImagesListViewPresenter()
+        let view = ImagesListViewControllerSpy()
+        presenter.view = view
+        
+        // when
+        presenter.viewDidLoad() // настраиваем observer
+        NotificationCenter.default.post(
+            name: ImagesListService.didChangeNotification,
+            object: nil
+        )
+        
+        // then - проверяем что не падает при нотификации
+        XCTAssertTrue(true)
+    }
+}
