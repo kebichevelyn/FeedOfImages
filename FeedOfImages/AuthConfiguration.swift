a2add98d1c7695117fb48964aa5bd439ae58a91a
evelina kebich <KebichEvelina@yandex.by>
2025-11-23 12:54:39 +0300

    приложение с тестами на webView работает корректно

--- FeedOfImages/AuthConfiguration.swift
+++ FeedOfImages/AuthConfiguration.swift
@@ -16,9 +16,9 @@
 import Foundation
 
 enum Constants {
-    static let accessKey = "your_Access_Key"
-    static let secretKey = "your_Secret_Key"
-    static let redirectURI = "your_Redirect_URI"
+    static let accessKey = "eVDMtRMJDuqPF1swn9g41VAyiEInUFGeBncwECEOm0E"
+    static let secretKey = "ci_UDDyeXPnv36BnsiZvU0L_JM2nwyK8xs-d5ZSWeh4"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
 
     static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
